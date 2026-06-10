6b3dfd77e34d93259c66c7454678bdee5540805d
David-Santiago-Gil <gilsantiagodepa@gmail.com>
2026-06-10 16:31:20 -0500

    chore: ignore and untrack environment configuration files

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://api.rawg.io/api',
-  apiKey: '9bc8625a52c5427a84ac74ba52b38050'
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://api.rawg.io/api',
-  apiKey: '9bc8625a52c5427a84ac74ba52b38050'
-};
